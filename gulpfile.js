8ec76e441f8752b67348b9909f9689de0f97172b
Taylor Kemper <taylor@revature.com>
2016-05-17 21:38:34 -0400

    remove comments in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,3 @@ fs.readdirSync(__dirname + '/gulp').forEach(function(task){
 });
 
 gulp.task('dev', ['watch:js', 'watch:css', 'dev:server', 'dev:mongo']);
-
-
-
-//gulp.task('js', function(){
-//	gulp.src('ng/**/*.js')
-//		.pipe(concat('app.js'))
-//		.pipe(gulp.dest('assets'));
-//});
-
-
-
-/*
-gulp.task('welcome', function(){
-	console.log('welcome to gulp!');
-});
-
-gulp.task('hello',['welcome'], function(){
-	console.log('hello world');
-});
-*/
