62e9482b19ed733d51c4e254be704d65acbbd64d
Taylor Kemper <taylor@revature.com>
2016-05-26 11:52:43 -0400

    add making_a_post.spec.js

---
+++ test/e2e/making_a_post.spec.js
@@ -0,0 +1,54 @@
+var expect = require('chai').expect;
+var db = require('../../db');
+
+
+describe('making a post', function(){
+	it('creates an account and a new post', function(){
+		//go to homepage
+		browser.get('http://localhost:3001');
+
+		//click 'login'
+		element(by.css('nav .register')).click();
+
+		//fill out and submit login form
+		element(by.model('registerCtrl.username')).sendKeys('john');
+		element(by.model('registerCtrl.password')).sendKeys('123');
+		element(by.id('registerBtn')).click();
+
+		//go to posts page
+		element(by.css('.posts')).click();
+
+		//submit a new post on the posts page
+		var post = 'post from protractor!' + Math.random();
+		element(by.model('postsCtrl.newMsg')).sendKeys(post);
+		element(by.id('addPostBtn')).click();
+
+		//the user should now see their post as the first post on the page
+		
+		//browser.waitForAngular();
+
+/*
+		element.all(by.css('ul.list-group li')).first().getText().then(function(text){
+			console.log(text);
+		});
+*/
+	
+		//expect(element(by.css('ul.list-group li')).isPresent()).toBeTruthy();
+
+		//browser.pause();
+		//browser.sleep(3000);
+
+		element.all(by.css('ul.list-group li')).first().getText().then(function(text){
+			//console.log(text);
+			expect(text).to.contain(post);
+		});
+		
+
+		//browser.pause();
+	});
+
+	afterEach(function(){
+
+		db.connection.db.dropDatabase();
+	});
+});
