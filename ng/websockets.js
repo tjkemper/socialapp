9d86ddc25acf8a74d80fcd935c820cfd5b1569ba
Taylor Kemper <tjkemper6@gmail.com>
2016-05-29 01:00:17 -0400

    modify ng WebSockets - created WebSocketService

--- ng/websockets.js
+++ ng/websockets.js
@@ -16,6 +16,8 @@ angular.module('SocialApp')
 
 });
 */
+
+/*
 angular.module('SocialApp')
 .run(function ($rootScope, $timeout){
 	(function connect(){
@@ -46,4 +48,34 @@ angular.module('SocialApp')
 		}	
 	})();
 	
+});
+*/
+
+angular.module('SocialApp')
+.service('WebSocketService', function($rootScope){
+	function websocketHost(){
+		if(window.location.protocol === "https:"){
+			return "wss://" + window.location.host;	
+		}else {
+			return "ws://" + window.location.host;	
+		}
+	}
+
+	var connection;
+	this.connect = function(){
+		connection = new WebSocket(websocketHost());
+		connection.onmessage = function(e){
+			var payload = JSON.parse(e.data);
+			$rootScope.$broadcast('ws:'+payload.topic, payload.data);
+		}
+	}
+
+	this.send = function(topic, data){
+		var json = JSON.stringify({topic: topic, data:data});
+		connection.send(json);
+	}
+
+})
+.run(function(WebSocketService){
+	WebSocketService.connect();
 });
