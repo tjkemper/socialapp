3d24987a41a28706011c5078e822de5fb35b5805
Taylor Kemper <tjkemper6@gmail.com>
2016-05-28 23:29:40 -0400

    add gulp task - build

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,6 @@ fs.readdirSync(__dirname + '/gulp').forEach(function(task){
 	require('./gulp/' + task);
 });
 
+gulp.task('build', ['js', 'css']);
+
 gulp.task('dev', ['watch:js', 'watch:css', 'watch:html', 'dev:server', 'dev:mongo']);
